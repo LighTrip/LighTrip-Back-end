ffa0e5bdbe48ab16e31d22b099d587142aa1f841
jaehyeon4406 <jaehyeon4406@gmail.com>
2026-03-28 12:07:22 +0900

    setting: lightrip 초기 세팅

---
+++ src/main/java/com/kauniv/lightrip/LightripApplication.java
@@ -0,0 +1,13 @@
+package com.kauniv.lightrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauniv/lightrip/LightripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauniv.lightrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
