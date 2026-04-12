4eafc9277bef44373ff86a3136104df5e1b226fd
이상원 <sangwon020214@gmail.com>
2026-04-12 12:44:32 +0900

    deploy: URL 추가 및 배포 브랜치 변경

--- src/main/java/com/kauniv/lightrip/global/config/SecurityConfig.java
+++ src/main/java/com/kauniv/lightrip/global/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/oauth2/**", "/login/**", "/auth/refresh").permitAll()
+                        .requestMatchers("/", "/oauth2/**", "/login/**", "/auth/refresh", "/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
