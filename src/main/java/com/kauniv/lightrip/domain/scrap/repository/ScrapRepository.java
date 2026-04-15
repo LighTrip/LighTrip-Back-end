f24c28bb1a7d0a47600f692799b039db279c959b
이상원 <sangwon020214@gmail.com>
2026-04-15 23:26:39 +0900

    feat: 여권(Passport) 등록/수정/삭제 API 구현

--- src/main/java/com/kauniv/lightrip/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/com/kauniv/lightrip/domain/scrap/repository/ScrapRepository.java
@@ -1,7 +1,15 @@
+// domain/scrap/repository/ScrapRepository.java
 package com.kauniv.lightrip.domain.scrap.repository;
 
 import com.kauniv.lightrip.domain.scrap.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+
+    // 여권 삭제 시 연관 스크랩 일괄 삭제 (성능 위해 bulk delete)
+    @Modifying
+    @Query("DELETE FROM Scrap s WHERE s.passport.id = :passportId")
+    void deleteAllByPassportId(Long passportId);
 }
