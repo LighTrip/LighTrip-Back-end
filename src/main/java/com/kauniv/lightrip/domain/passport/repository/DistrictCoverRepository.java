39d8a409c946bfad12019a89f4078a2fe692e7c8
이상원 <sangwon020214@gmail.com>
2026-05-02 23:34:36 +0900

    feat: DistrictCover 엔티티 추가 + 여권 등록/삭제 시 자동 생성/정리

---
+++ src/main/java/com/kauniv/lightrip/domain/passport/repository/DistrictCoverRepository.java
@@ -0,0 +1,16 @@
+package com.kauniv.lightrip.domain.passport.repository;
+
+import com.kauniv.lightrip.domain.passport.entity.DistrictCover;
+import com.kauniv.lightrip.global.enums.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DistrictCoverRepository extends JpaRepository<DistrictCover, Long> {
+
+    Optional<DistrictCover> findByUser_IdAndDistrictCategory(Long userId, District districtCategory);
+
+    boolean existsByUser_IdAndDistrictCategory(Long userId, District districtCategory);
+
+    void deleteByUser_IdAndDistrictCategory(Long userId, District districtCategory);
+}
