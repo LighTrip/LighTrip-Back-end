3d97e2ea57d6e0a4b2d75c99733face8ef8a563e
jaehyeon4406 <jaehyeon4406@gmail.com>
2026-05-11 00:18:57 +0900

    feat: S3 Presigned URL 및 CloudFront CDN 연동

---
+++ src/main/java/com/kauniv/lightrip/global/s3/service/S3Service.java
@@ -0,0 +1,58 @@
+package com.kauniv.lightrip.global.s3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.cloudfront.domain}")
+    private String cloudfrontDomain;
+
+    // Presigned URL 생성 (업로드용)
+    public String generatePresignedUploadUrl(String s3Key, String contentType) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(s3Key)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(putObjectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+        return presignedRequest.url().toString();
+    }
+
+    // CloudFront URL 반환 (조회용)
+    public String getCloudFrontUrl(String s3Key) {
+        return "https://" + cloudfrontDomain + "/" + s3Key;
+    }
+
+    // S3 파일 삭제
+    public void deleteFile(String s3Key) {
+        DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(s3Key)
+                .build();
+        s3Client.deleteObject(deleteRequest);
+    }
+}
