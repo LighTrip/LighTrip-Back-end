8b90312a90d71e6dda0dd39a7378aaaa50a41a31
이상원 <sangwon020214@gmail.com>
2026-05-26 23:11:54 +0900

    feat: 결제 도메인 엔티티 및 마이그레이션 추가

---
+++ src/main/java/com/kauniv/lightrip/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.kauniv.lightrip.domain.payment.repository;
+
+import com.kauniv.lightrip.domain.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByOrderId(String orderId);
+}
